6d451e5a55264cecd96e8758c4bc111e564db30f
hoethan231 <Icydragons1357@gmail.com>
2024-06-27 23:54:30 -0700

    add scripts, font, and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className='title'>
+                <h1>Ethan Ho</h1>
+                <h2>Second Year in <br/>Computer Engineering</h2>
+            </div>
+        </div>
+    );
 }
 
 export default App;
