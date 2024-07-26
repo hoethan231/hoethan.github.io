79a2fa23864f1cdf9b7584426664f82f3e75561c
hoethan231 <Icydragons1357@gmail.com>
2024-07-26 01:07:15 -0700

    create experiences tab and upload picture

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Hero from './components/Hero/Hero.jsx';
+import Experience from './components/Experience/Experience.jsx';
+import './App.css';
 
 function App() {
     return (
         <div className="App">
             <Navbar/>
             <Hero/>
+            <Experience/>
         </div>
     );
 }
---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Experience.css";
+
+const Experience = () => {
+
+    return (
+        <div className="xp-container">
+        </div>
+    )
+}
+
+export default Experience;
