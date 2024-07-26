23a170929501f8e64dab1b207f1032a33f687822
hoethan231 <Icydragons1357@gmail.com>
2024-07-25 20:38:57 -0700

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar.jsx';
 
 function App() {
     return (
         <div className="App">
-            <div className='title'>
-                <h1>Ethan Ho</h1>
-                <h2>Second Year in <br/>Computer Engineering</h2>
-            </div>
+            <Navbar/>
         </div>
     );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Navbar.css"
+
+const Navbar = () => {
+
+    return (
+        <div className="navbar">
+            <h1>Ethan</h1>
+            <ul className="nav-menu">
+                <li>Home</li>
+                <li>About Me</li>
+                <li>Projects</li>
+                <li>Contacts</li>
+            </ul>
+        </div>
+    )
+
+}
+
+export default Navbar;
