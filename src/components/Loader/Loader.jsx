0ea101bdf8869623e5964e19adcff55876d555b0
hoethan231 <Icydragons1357@gmail.com>
2024-08-04 01:25:24 -0700

    implement preloader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+import "./Loader.css";
+
+const Loader = () => {
+
+    return (
+        <div className="pre-loader">
+            <div className="text-box">
+                <p className="text">Ethan Ho.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Loader;
