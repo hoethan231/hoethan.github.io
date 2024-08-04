9076e8d8f1e2645e41608f52ebc76e71f22a6549
hoethan231 <Icydragons1357@gmail.com>
2024-08-04 01:26:20 -0700

    fix background color issues

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import Navbar from './components/Navbar/Navbar.jsx';
 import Hero from './components/Hero/Hero.jsx';
 import Projects from './components/Projects/Projects.jsx';
+import Loader from './components/Loader/Loader.jsx';
 import './App.css';
 
 function App() {
+
     return (
-        <div className="App">
-            <Navbar/>
-            <Hero/>
-            <Projects/>
+        <div className="app-container">
+            {/* <Loader /> */}
+            <Hero />
+            <Projects />
         </div>
     );
-}
+} 
 
 export default App;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import Navbar from '../Navbar/Navbar.jsx';
 import pfp from "../../assets/pfp.jpg"
 import "./Hero.css";
 
 const Hero = () => {
 
     return (
-        <div className="hero">
-            <div className="leftside">
-                <img src={pfp} alt=""></img>
-            </div>
-            <div className="rightside">
-                <h1 className="name">ETHAN HO</h1>
-                <p className="subtext">Aspiring Software Engineer</p>
+        <div className="herocontainer">
+            <Navbar />
+            <div className="hero">
+                <div className="leftside">
+                    <img src={pfp} alt=""></img>
+                </div>
+                <div className="rightside">
+                    <h1 className="name">ETHAN HO</h1>
+                    <p className="subtext">Student in <br/>Computer Engineering</p>
+                </div>
             </div>
         </div>
     )
